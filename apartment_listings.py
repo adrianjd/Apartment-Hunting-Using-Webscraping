8e9cf0dc594dd8c827a9c776905086117122988c
adrianjd <84035528+adrianjd@users.noreply.github.com>
2023-01-23 22:29:13 -0500

    Update apartment_listings.py
    Added code to write info into csv file. Added extra column header ('website') for easier reading.  Added additional comments. Added final function calls.

--- apartment_listings.py
+++ apartment_listings.py
@@ -25,37 +25,44 @@ def find_realtor_listings():
     listings = soup.find_all('div', class_= 'CardContent__StyledCardContent-rui__sc-7ptz1z-0 echMdB card-content')
     
     # Input each listing into a file
-    for listing in listings:
-        price = listing.find('div', class_= 'price-wrapper').text
-        location = (
-            f"{listing.find('div', attrs={'data-testid': 'card-address-1'}).text}"
-            ", "
-            f"{listing.find('div', attrs={'data-testid': 'card-address-2'}).text}"
-        )
+    with open('apartments.csv', 'w', encoding='utf8', newline='') as f:
+        output_writer = csv.writer(f)
+        headers = ['Website', 'Price', 'Location', 'Beds', 'Baths', 'Size', 'Pets'] # Column headers for csv file
+        output_writer.writerow(headers)
         
-        bed = listing.find('li', class_= 'styles__StyledPropertyBedMeta-rui__jbdr1y-0 gUHOjr').text
-        bath = listing.find('li', class_= 'styles__StyledPropertyBathMeta-rui__sc-6egb6z-0 dqxXcq').text
+        for listing in listings:
+            website = "realtor.com"
+            price = listing.find('div', class_= 'price-wrapper').text
+            location = (
+                f"{listing.find('div', attrs={'data-testid': 'card-address-1'}).text}"
+                ", "
+                f"{listing.find('div', attrs={'data-testid': 'card-address-2'}).text}"
+            )
 
-        # Some listings don't have area size or pet info readily available
-        # Us try, except to handle missing attributes
-        try:
-            area = (
-                listing.find('li', class_= 'styles__StyledPropertySqftMeta-rui__sc-1f5nqhv-0 hZvqmo')
-                .find('span', class_='styles__StyledVisuallyHidden-rui__sc-1otsbow-0 kvJPgr').text
-            )
-        except AttributeError:
-            area = "Info N/A"
-        
-        try:
-            pets = (
-                listing.find('li', class_= 'styles__StyledPropertyPetMeta-rui__sc-1ebu08q-0 bWZehQ')
-                .find('span', class_= 'styles__StyledVisuallyHidden-rui__sc-1otsbow-0 kvJPgr').text
-            )
-        except AttributeError:
-            pets = "Info N/A"
-            
-        
-        info = [price, location, bed, bath, area, pets]
+            bed = listing.find('li', class_= 'styles__StyledPropertyBedMeta-rui__jbdr1y-0 gUHOjr').text
+            bath = listing.find('li', class_= 'styles__StyledPropertyBathMeta-rui__sc-6egb6z-0 dqxXcq').text
+
+            # Some listings don't have area size or pet info readily available
+            # Us try, except to handle missing attributes
+            try:
+                area = (
+                    listing.find('li', class_= 'styles__StyledPropertySqftMeta-rui__sc-1f5nqhv-0 hZvqmo')
+                    .find('span', class_='styles__StyledVisuallyHidden-rui__sc-1otsbow-0 kvJPgr').text
+                )
+            except AttributeError:
+                area = "Info N/A"
+
+            try:
+                pets = (
+                    listing.find('li', class_= 'styles__StyledPropertyPetMeta-rui__sc-1ebu08q-0 bWZehQ')
+                    .find('span', class_= 'styles__StyledVisuallyHidden-rui__sc-1otsbow-0 kvJPgr').text
+                )
+            except AttributeError:
+                pets = "Info N/A"
+
+
+            info = [website, price, location, bed, bath, area, pets]
+            output_writer.writerow(realtor_info)
         
 
 def find_redfin_listings():
@@ -65,22 +72,27 @@ def find_redfin_listings():
     listings = soup.find_all('div', class_= 'MapHomeCardReact HomeCard')
     
     # Input each listing into a file
-    for listing in listings:
-        price = listing.find('span', class_= 'homecardV2Price').text
-        location = listing.find('div', class_= 'homeAddressV2').text
-        bed = listing.find('div', class_= 'HomeStatsV2 font-size-small').find_all('div', class_='stats')[0].text
-        bath = listing.find('div', class_= 'HomeStatsV2 font-size-small').find_all('div', class_='stats')[1].text
-        area = listing.find('div', class_= 'HomeStatsV2 font-size-small').find_all('div', class_='stats')[2].text
-        if area == "—Sq. Ft.":
-            area = "Info N/A"
-                
-        # Pet info is not readily available in Redfin, so '----' is just dummy text to trigger the try except
-        try:
-            pets = listing.find('div', class_= '----').text
-        except AttributeError:
-            pets = "Info N/A"
+    with open('apartments.csv', 'a', encoding='utf8', newline='') as f:
+        output_writer = csv.writer(f)
+        
+        for listing in listings:
+            website = "redfin.com"
+            price = listing.find('span', class_= 'homecardV2Price').text
+            location = listing.find('div', class_= 'homeAddressV2').text
+            bed = listing.find('div', class_= 'HomeStatsV2 font-size-small').find_all('div', class_='stats')[0].text
+            bath = listing.find('div', class_= 'HomeStatsV2 font-size-small').find_all('div', class_='stats')[1].text
+            area = listing.find('div', class_= 'HomeStatsV2 font-size-small').find_all('div', class_='stats')[2].text
+            if area == "—Sq. Ft.":
+                area = "Info N/A"
 
-        redfin_info = [price, location, bed, bath, area, pets]
+            # Pet info is not readily available in Redfin, so '----' is just dummy text to trigger the try except
+            try:
+                pets = listing.find('div', class_= '----').text
+            except AttributeError:
+                pets = "Info N/A"
+
+            redfin_info = [website, price, location, bed, bath, area, pets]
+            output_writer.writerow(redfin_info)
 
 def find_zillow_listings():
     """Find apartments in Belleville, NJ on zillow.com"""
@@ -89,44 +101,53 @@ def find_zillow_listings():
     listings = soup.find_all('li', class_= 'ListItem-c11n-8-81-1__sc-10e22w8-0 srp__hpnp3q-0 enEXBq with_constellation')
     
     # Input each listing into a file
-    for listing in listings:
-        try:
-         price = listing.find('div', class_= 'StyledPropertyCardDataArea-c11n-8-81-1__sc-yipmu-0 wgiFT').text
-        except:
-            continue
+    with open('apartments.csv', 'a', encoding='utf8', newline='') as f:
+        output_writer = csv.writer(f)
+    
+        for listing in listings:
+            website = "zillow.com"
+            try:
+                price = listing.find('div', class_= 'StyledPropertyCardDataArea-c11n-8-81-1__sc-yipmu-0 wgiFT').text
+            except:
+                continue
 
-        location = listing.find('a', class_= 'StyledPropertyCardDataArea-c11n-8-81-1__sc-yipmu-0 lpqUkW property-card-link').text
-        bed = (
-            listing.find('ul', class_= 'StyledPropertyCardHomeDetailsList-c11n-8-81-1__sc-1xvdaej-0 cBiTXE')
-            .find_all('li')[0].text
-        )
-        
-        try:
-            bath = (
+            location = listing.find('a', class_= 'StyledPropertyCardDataArea-c11n-8-81-1__sc-yipmu-0 lpqUkW property-card-link').text
+            bed = (
                 listing.find('ul', class_= 'StyledPropertyCardHomeDetailsList-c11n-8-81-1__sc-1xvdaej-0 cBiTXE')
-                .find_all('li')[1].text
+                .find_all('li')[0].text
             )
-        except:
-            bath = "Info N/A"
-        if "bds" in bath:
-            bath = "Info N/A"
-        
-       
-        try:
-            area = (
-                listing.find('ul', class_= 'StyledPropertyCardHomeDetailsList-c11n-8-81-1__sc-1xvdaej-0 cBiTXE')
-                .find_all('li')[2].text
-            )
-        except:
-            area = "Info N/A"
-        if "--" in area:
-            area = "Info N/A"
-        
-        # Pet info is not readily available in Zillow, so '----' is just dummy text to trigger the try except
-        try:
-            pets = listing.find('div', class_= '----').text
-        except AttributeError:
-            pets = "Info N/A"
 
-        zillow_info = [price, location, bed, bath, area, pets]
+            try:
+                bath = (
+                    listing.find('ul', class_= 'StyledPropertyCardHomeDetailsList-c11n-8-81-1__sc-1xvdaej-0 cBiTXE')
+                    .find_all('li')[1].text
+                )
+            except:
+                bath = "Info N/A"
+            if "bds" in bath:
+                bath = "Info N/A"
 
+
+            try:
+                area = (
+                    listing.find('ul', class_= 'StyledPropertyCardHomeDetailsList-c11n-8-81-1__sc-1xvdaej-0 cBiTXE')
+                    .find_all('li')[2].text
+                )
+            except:
+                area = "Info N/A"
+            if "--" in area:
+                area = "Info N/A"
+
+            # Pet info is not readily available in Zillow, so '----' is just dummy text to trigger the try except
+            try:
+                pets = listing.find('div', class_= '----').text
+            except AttributeError:
+                pets = "Info N/A"
+
+            zillow_info = [website, price, location, bed, bath, area, pets]
+            output_writer.writerow(zillow_info)
+
+print(os.getcwd()) # Print this to find working directory for created csv file; comment out if not needed
+find_realtor_listings()
+find_redfin_listings()
+find_zillow_listings()
